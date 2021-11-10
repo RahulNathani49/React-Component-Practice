6e86f4d3ef085874c8279b091663492236372cc8
RahulNathani49 <nathanirahul49@gmail.com>
2021-11-10 16:30:16 -0500

    React Basic function Component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Footer(){
+    return(
+        <div>
+            <h1>Footer</h1>
+            <p>Copyrights section  @React Basics</p>
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+function Header(){
+    return(
+        <div>
+            <nav>
+                <h1>Header</h1>
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Header
